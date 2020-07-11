1a62a9e2fffaf9d7f100308f6fb48df220aa2949
Angel X. Moreno <angelxmoreno@gmail.com>
2020-07-11 08:25:35 -0400

    Added and integrated native base

--- App.tsx
+++ App.tsx
@@ -1,28 +1,52 @@
-import {StatusBar} from 'expo-status-bar';
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {Component} from 'react';
 import CounterUI from "./src/components/CounterUI";
 import {store, StoreContext} from "./src/stores";
 import 'mobx-react-lite/batchingForReactDom'
+import {AppLoading} from 'expo';
+import {Body, Container, Header, Left, Right, Root, Subtitle, Text, Title} from 'native-base';
+import * as Font from 'expo-font';
+import {Ionicons} from '@expo/vector-icons';
 
-export default function App() {
-    return (
-        <StoreContext.Provider value={store}>
-            <View style={styles.container}>
-                <Text>Open up App.js to start working on your app!</Text>
-                <StatusBar style="auto"/>
+export default class App extends Component<any, any> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isReady: false,
+        };
+    }
 
-                <CounterUI/>
-            </View>
-        </StoreContext.Provider>
-    );
+    async componentDidMount() {
+        await Font.loadAsync({
+            Roboto: require('native-base/Fonts/Roboto.ttf'),
+            Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+            ...Ionicons.font,
+        });
+        this.setState({isReady: true});
+    }
+
+    render() {
+        if (!this.state.isReady) {
+            return <AppLoading/>;
+        }
+
+        return (
+            <StoreContext.Provider value={store}>
+                <Root>
+                    <Container>
+                        <Header>
+                            <Left/>
+                            <Body>
+                                <Title>Title</Title>
+                                <Subtitle>Subtitle</Subtitle>
+                            </Body>
+                            <Right/>
+                        </Header>
+                        <Text>Open up App.js to start working on your app!</Text>
+                        <CounterUI/>
+                    </Container>
+                </Root>
+            </StoreContext.Provider>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
